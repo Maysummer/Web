4a219abe913f0f4bcdca7adc48fa7aeb0075b849
Maysummer <udojyk@gmail.com>
2023-12-15 18:15:39 +0100

    add JSON server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Nmeso', age: 120 });
 
-user.set({name: 'ngozi'})
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+  console.log('change 1')
+});
+
+user.on('change', () => {
+  console.log('change 2')
+});
+
+user.on('save', () => {
+  console.log('save 1')
+});
+
+user.trigger('save')
