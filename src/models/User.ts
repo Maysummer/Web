2c2f01dbf65ec97ef54fc00c6be518bfbd67441c
Maysummer <udojyk@gmail.com>
2023-12-18 12:16:17 +0100

    extract sync implementation

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,3 @@
-import axios, { AxiosResponse } from 'axios';
 
 interface UserProps {
   id?: number;
@@ -19,21 +18,5 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data)
-    } else {
-      axios.post('http://localhost:3000/users/', this.data)
-    }
-  }
+  
 }
